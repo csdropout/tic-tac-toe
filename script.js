4bd34d82d1d5baf8184de58a033cc4b9b52fe0fa
qile <98936712+csdropout@users.noreply.github.com>
2026-03-05 22:42:45 +0800

    Refactor check win and tie
    Fix checkWin function
    Add check game state function

--- script.js
+++ script.js
@@ -29,35 +29,13 @@ function GameBoard() {
         return !!board[row][col];
     }
 
-    const isWinner = (row, col, marker) => {
-        // check across (current row)
-        if (board[row].filter((x) => x === marker).length === 3) return true;
-
-        // check vertical (current col)
-        const verticalCol = [];
-        board.forEach((row) => {
-            verticalCol.push(row[col]);
-        });
-        if (verticalCol.filter((x) => x === marker).length === 3) return true;
-
-        // check diagonals (marker place in corners or in the middle)
-        // 2 diagonals
-        if (board[0][0] === marker && board[1][1] === marker && board[2][0]) return true;
-        if (board[2][0] === marker && board[1][1] === marker && board[0][2]) return true;
-        
-        return false;
-    }
-
-    const isTie = () => {
-        return board.flat(Infinity).filter((x) => x === "").length === 0? true : false;
-    }
-
-    return {getBoard, printBoard, markCell, isCellMarked, isWinner, resetBoard, isTie}
+    return {getBoard, printBoard, markCell, isCellMarked, resetBoard}
 }
 
 function GameController(playerOneName = "P1", playerOneMarker = "O", playerTwoName = "P2", playerTwoMarker = "X") {
     const board = GameBoard();
-    
+    let gameOver = false;
+
     const players = [
         {name: playerOneName, marker: playerOneMarker},
         {name: playerTwoName, marker: playerTwoMarker}
@@ -83,10 +61,12 @@ function GameController(playerOneName = "P1", playerOneMarker = "O", playerTwoNa
             console.log("Invalid move!")
         }
         
-        if (board.isWinner(row, col, activePlayer.marker)) {
+        if (checkWin(row, col, activePlayer.marker)) {
             console.log(`${activePlayer.name} has won!`);
-        } else if (board.isTie()) {
+            gameOver = true;
+        } else if (checkTie()) {
             console.log("Game ended in a tie!");
+            gameOver = true;
         } else {
             changeActivePlayer();
             printNewRound();
@@ -97,13 +77,42 @@ function GameController(playerOneName = "P1", playerOneMarker = "O", playerTwoNa
         console.log("Restarting game...");
         board.resetBoard();
         activePlayer = players[0];
+        gameOver = false;
         printNewRound();
     }
+
+    const checkWin = (row, col, marker) => {
+        const b = board.getBoard()
+        // check across (current row)
+        if (b[row].filter((x) => x === marker).length === 3) {
+            return true;
+        }
+
+        // check vertical (current col)
+        const verticalCol = [];
+        b.forEach((row) => {
+            verticalCol.push(row[col]);
+        });
+        if (verticalCol.filter((x) => x === marker).length === 3) return true;
+
+        // check diagonals (marker place in corners or in the middle)
+        // 2 diagonals
+        if (b[0][0] === marker && b[1][1] === marker && b[2][2] === marker) return true;
+        if (b[2][0] === marker && b[1][1] === marker && b[0][2] === marker) return true;
+        
+        return false;
+    }
+
+    const checkTie = () => {
+        return board.getBoard().flat(Infinity).filter((x) => x === "").length === 0? true : false;
+    }
+
+    const getGameOverStatus = () => gameOver;
 
     // Start of new game
     printNewRound();
 
-    return {playRound, getActivePlayer, restart, getBoard: board.getBoard}
+    return {playRound, getActivePlayer, restart, getBoard: board.getBoard, getGameOverStatus}
 }
 
 const screenController = (() => {
@@ -136,8 +145,12 @@ const screenController = (() => {
     boardDiv.addEventListener('click', (e) => {
         const selectedRow = e.target.row;
         const selectedCol = e.target.col;
-        game.playRound(selectedRow, selectedCol);
-        updateScreen(); 
+        if (!game.getGameOverStatus()) {
+            game.playRound(selectedRow, selectedCol);
+            updateScreen(); 
+        } else {
+            console.log("Game is over. Please start a new game.")
+        }
     })
 
     updateScreen();
